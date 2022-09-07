5b7e10599eb7f12f61ef7bed4bf1c00344c35db0
Swaroop Nadella <swaroopnadella@hotmail.com>
2022-09-07 23:44:55 +0530

    Handling Shadow DOM elements on Chrome downloads page

---
+++ src/main/java/handleShadowDOM/shadowRootChromeDownloads.java
@@ -0,0 +1,28 @@
+package handleShadowDOM;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class shadowRootChromeDownloads {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("chrome://downloads/");
+		
+		JavascriptExecutor jse = (JavascriptExecutor)driver;
+		WebElement search = (WebElement) jse.executeScript("return document.querySelector('downloads-manager').shadowRoot.querySelector('downloads-toolbar#toolbar').shadowRoot.querySelector('cr-toolbar#toolbar').shadowRoot.querySelector('cr-toolbar-search-field#search').shadowRoot.querySelector('div#searchTerm > input#searchInput')");
+		
+		String js = "arguments[0].setAttribute('value','SwaroopNadella')";
+		((JavascriptExecutor)driver).executeScript(js, search);
+		
+		
+	}
+
+}
