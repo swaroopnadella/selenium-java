f09a0d3b45cd0f95ebd95eaefd6f9d2df1b3a15f
Swaroop Nadella <swaroopnadella@hotmail.com>
2022-09-11 14:07:49 +0530

    handling shadow-root elements using JavascriptExecutor

---
+++ src/main/java/handleShadowDOM/shadowRootGetText.java
@@ -0,0 +1,31 @@
+package handleShadowDOM;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class shadowRootGetText {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://shop.polymer-project.org/");
+
+		JavascriptExecutor jse = (JavascriptExecutor) driver;
+		WebElement shopHome = (WebElement)  jse.executeScript(
+				"return document.querySelector(\"body > shop-app\").shadowRoot.querySelector(\"#header > app-toolbar > div.logo > a\")");
+		String text = shopHome.getText();
+		System.out.println(text);
+		String aria_label_text = shopHome.getAttribute("aria-label");
+		System.out.println(aria_label_text);
+		
+		driver.close();
+	}
+
+}
---
+++ src/main/java/handleShadowDOM/shadowRootSearchBooks.java
@@ -0,0 +1,41 @@
+package handleShadowDOM;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class shadowRootSearchBooks {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://books-pwakit.appspot.com/");
+
+		JavascriptExecutor jse = (JavascriptExecutor) driver;
+		
+		WebElement pageTitle = (WebElement) jse.executeScript(
+				"return document.querySelector(\"body > book-app\").shadowRoot.querySelector(\"app-header > app-toolbar.toolbar-top > div > a\")");
+		System.out.println(pageTitle.getText());
+		
+		WebElement search = (WebElement) jse.executeScript(
+				"return document.querySelector(\"body > book-app\").shadowRoot.querySelector(\"#input\")");
+		search.sendKeys("Test Automation");
+		search.sendKeys(Keys.ENTER);
+		Thread.sleep(5000);
+		
+		WebElement firstSearchResultText = (WebElement) jse.executeScript(
+				"return document.querySelector(\"body > book-app\").shadowRoot.querySelector(\"main > book-explore\").shadowRoot.querySelector(\"section > ul > li:nth-child(1) > book-item\").shadowRoot.querySelector(\"a > div.info > div > div.title-container > h2\")");
+		System.out.println(firstSearchResultText.getText());
+		
+
+		driver.quit();
+	}
+
+}
