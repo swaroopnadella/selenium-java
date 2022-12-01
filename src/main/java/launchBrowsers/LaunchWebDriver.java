798c1902258a8a8219b479c934ceac8c2bd6737b
Swaroop Nadella <swaroopnadella@hotmail.com>
2022-12-01 23:08:10 +0530

    Creating WebDriver object not allowed as it is an Interface

---
+++ src/main/java/launchBrowsers/LaunchWebDriver.java
@@ -0,0 +1,16 @@
+package launchBrowsers;
+
+import org.openqa.selenium.WebDriver;
+
+public class LaunchWebDriver {
+
+	public static void main(String[] args) {
+		
+		//WebDriver driver = new WebDriver();
+		//uncomment the above line to get compile time error "Cannot instantiate the type WebDriver"
+		//It is not possible to create object for WebDriver which is an Interface
+		
+		
+	}
+
+}
