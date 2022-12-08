5c1bd16e0c21603e4bd45b5b02d4cb639bace7d5
Swaroop Nadella <swaroopnadella@hotmail.com>
2022-12-09 00:46:45 +0530

    Can we write ChromeDriver driver = new WebDriver() ?

---
+++ src/main/java/launchBrowsers/LaunchChromeDriverWithWebDriver2.java
@@ -0,0 +1,14 @@
+package launchBrowsers;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+//Added below import step for WebDriver manually
+import org.openqa.selenium.WebDriver;
+
+public class LaunchChromeDriverWithWebDriver2 {
+
+	public static void main(String[] args) {
+		
+		//ChromeDriver driver = new WebDriver();  
+		//uncomment above line to get compiler error "Cannot instantiate the type WebDriver"
+	}
+}
--- src/main/java/launchBrowsers/launchChromeDriverWithWebDriver.java
+++ src/main/java/launchBrowsers/launchChromeDriverWithWebDriver.java
@@ -1,20 +1,12 @@
 package launchBrowsers;
 
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.WebDriver;
 
-public class launchChromeDriverWithWebDriver {
+public class LaunchChromeDriverWithWebDriver {
 
 	public static void main(String[] args) {
-		WebDriver driver1; //just added to add the import statement for WebDriver
-		//ChromeDriver driver = new WebDriver();
-		// uncomment above line to get compile time error "Cannot instantiate the type WebDriver"
-		// 1.Using child class instance we cannot access the parent And
-		// 2.WebDriver is an Interface so not possible to call constructor or create object
-
-		// add the import statement for WebDriver manually otherwise we get error as "WebDriver cannot be resolved to a type"
-		// as WebDriver do not have a constructor since it is an Interface
-		// import org.openqa.selenium.WebDriver;
+		
+		//ChromeDriver driver = new WebDriver(); 
+		//uncomment above line to get compiler error "WebDriver cannot be resolved to a type"
 	}
 }
